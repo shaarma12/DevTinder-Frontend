e7e25ab59cefd5fe278380adfa5b750e81ffe105
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2025-02-18 17:31:42 +0530

    setup the routing and creating dummy component page - Profile, Login, Feed

---
+++ src/Components/Feed.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-semibold mt-10">
+        This is Feed Page
+      </h1>
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-semibold mt-10">
+        This is Profile Page
+      </h1>
+    </div>
+  );
+};
+
+export default Profile;
